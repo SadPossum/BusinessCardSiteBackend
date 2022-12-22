d2cca35175d07e7046a82032ed997e78fc11a763
SadPossum <258739@bk.ru>
2022-12-22 17:30:40 +0300

    feat: Add value validation to Review model

--- BusinessCardSiteBackendDemo/Models/Review.cs
+++ BusinessCardSiteBackendDemo/Models/Review.cs
@@ -1,12 +1,27 @@
-﻿namespace BusinessCardSiteBackendDemo.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BusinessCardSiteBackendDemo.Models
 {
     public class Review
     {
         public int Id { get; set; }
+
+        [Required]
         public string AuthorName { get; set; } = "";
+
+        [StringLength(256)]
         public string AuthorContacts { get; set; } = "";
+
+        [StringLength(1024)]
         public string AuthorInformation { get; set; } = "";
+
+        [Required]
+        [StringLength(1024)]
         public string SubjectDescription { get; set; } = "";
+
+        [Required]
+        [StringLength(1024)]
         public string SubjectOpinion { get; set; } = "";
     }
 }
+
