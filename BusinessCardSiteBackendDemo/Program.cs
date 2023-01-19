853c8eade32f70609a8fbf77737017001131f5e3
SadPossum <258739@bk.ru>
2023-01-19 22:25:11 +0300

    fix: Return Cors in builder.Services

--- BusinessCardSiteBackendDemo/Program.cs
+++ BusinessCardSiteBackendDemo/Program.cs
@@ -12,6 +12,8 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.
                        GetConnectionString("DefaultConnection")));
 
+builder.Services.AddCors();
+
 builder.Services.AddScoped<IReviewRepository, ReviewRepository>();
 builder.Services.AddScoped<ApplicationDbContext>();
 
@@ -21,7 +23,7 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseDeveloperExceptionPage();
+  app.UseDeveloperExceptionPage();
 }
 
 app.UseCors(p => p.AllowAnyOrigin()
@@ -37,7 +39,7 @@ app.UseAuthorization();
 
 app.UseEndpoints(endpoints =>
 {
-    endpoints.MapControllers();
+  endpoints.MapControllers();
 });
 
 app.Run();
