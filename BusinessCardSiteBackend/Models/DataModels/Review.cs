aa07c5b8ff5f4a3e93748e619f2a0d46b72d85d6
SadPossum <258739@bk.ru>
2023-02-12 23:10:14 +0300

    refactor: Replace "" with string.Empty

--- BusinessCardSiteBackend/Models/DataModels/Review.cs
+++ BusinessCardSiteBackend/Models/DataModels/Review.cs
@@ -3,11 +3,11 @@ namespace BusinessCardSiteBackend.Models.DataModels
     public class Review
     {
         public int Id { get; set; }
-        public string AuthorName { get; set; } = "";
-        public string AuthorContacts { get; set; } = "";
-        public string AuthorInformation { get; set; } = "";
-        public string SubjectDescription { get; set; } = "";
-        public string SubjectOpinion { get; set; } = "";
+        public string AuthorName { get; set; } = string.Empty;
+        public string AuthorContacts { get; set; } = string.Empty;
+        public string AuthorInformation { get; set; } = string.Empty;
+        public string SubjectDescription { get; set; } = string.Empty;
+        public string SubjectOpinion { get; set; } = string.Empty;
     }
 }
 
